9eca4f366d4ad93aa7cef7de2cf32a79c42c6d16
Invizabel <46697708+Invizabel@users.noreply.github.com>
2022-11-04 09:19:19 -0500

    Add files via upload

---
+++ ISS/ISS.py
@@ -0,0 +1,21 @@
+import re
+import requests
+import time
+import turtle
+
+screen = turtle.Screen()
+screen.setup(1920, 1080)
+screen.setworldcoordinates(-180, -90, 180, 90)
+
+screen.bgpic("map.gif")
+screen.register_shape("iss.gif")
+iss = turtle.Turtle()
+iss.shape("iss.gif")
+iss.setheading(45)
+iss.penup()
+
+while True:
+    result = requests.get("http://api.open-notify.org/iss-now.json").text
+    result = re.findall("\d+\.\d+", result)
+    iss.goto(float(result[0]), float(result[1]))
+    time.sleep(15)
